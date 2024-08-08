de1e91c8e46af9cce7c1e194858fa158e1b758f3
marcos_shift <marcosviniciussiqueira65@gmail.com>
2024-08-08 09:52:39 -0300

    function isPalindrome(); function arrayMaxMin
    realizei a primeira função para inverter palavras /frases, e se o parâmetro recebido for um palíndromo retornar true, caso ao contrário retornar false;
    
    segunda função retorna os números, máximo e minimo do parâmetro.

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(frase){
+
+    frase = frase.toLowerCase(); 
+    frase = frase.replace(/[^a-z0-9]/g, ''); //Remover espaços e pontuação da frase
+
+    let fraseInvertida = '';
+
+    for (let i = frase.length - 1; i >= 0; i--) {
+        fraseInvertida += frase[i];
+
+    }
+    return frase === fraseInvertida;
 }
+console.log(isPalindrome());
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+
+    let minimo = arr[0];
+    let maximo = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < minimo) {
+            minimo = arr[i];
+        }
+        if (arr[i] > maximo) {
+            maximo = arr[i]; 
+        }
+    }
+    return [minimo, maximo];
 }
+console.log(arrayMaxMin(minimo, maximo));
